02ce3503b12a919c369aaffa598a84ca31538d1a
Brady Rhora <brady0423@gmail.com>
2019-04-27 07:34:59 -0400

    changed a stupid way of doing things in SetData and turned debug mode off

--- ForkBot/Classes/Objects/User.cs
+++ ForkBot/Classes/Objects/User.cs
@@ -54,7 +54,7 @@ namespace ForkBot
             GetData(data); // ensure that the data exists
             for (int i = 0; i < uData.Count(); i++)
             {
-                if (uData[i] == data)
+                if (uData[i].Split(':')[0] == data)
                 {
                     uData[i] = uData[i].Substring(0, uData[i].IndexOf(':') + 1) + value;
                     Save(uData);
