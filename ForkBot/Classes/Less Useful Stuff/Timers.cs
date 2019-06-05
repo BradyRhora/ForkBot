8a2855f5f1a9350469be6b89b71bfea67672b500
Brady Rhora <brady0423@gmail.com>
2019-06-04 20:34:12 -0400

    more bids almost done but i wanna go home

--- ForkBot/Classes/Less Useful Stuff/Timers.cs
+++ ForkBot/Classes/Less Useful Stuff/Timers.cs
@@ -55,14 +55,73 @@ namespace ForkBot
             List<string> expiringSoon = new List<string>();
             foreach(string post in posts)
             {
-                var expiryDate = Functions.StringToDateTime(post.Split('|')[5]);
+                var expiryDate = Functions.StringToDateTime(post.Split('|')[5]) + new TimeSpan(14,0,0,0);
                 if (expiryDate - Var.CurrentDate() < new TimeSpan(0)) expired.Add(post);
-                else if (expiryDate - Var.CurrentDate() < new TimeSpan(1,0,0,0)) expired.Add(post);
+                else if (expiryDate - Var.CurrentDate() < new TimeSpan(1, 0, 0, 0))
+                {
+                    string id = post.Split('|')[0];
+                    if (Properties.Settings.Default.warnedFMs == null) Properties.Settings.Default.warnedFMs = new System.Collections.Specialized.StringCollection();
+                    if (!Properties.Settings.Default.warnedFMs.Contains(id))
+                    {
+                        Properties.Settings.Default.warnedFMs.Add(id);
+                        Properties.Settings.Default.Save();
+                        expiringSoon.Add(post);
+                    }
+
+                }
+            }
+            
+            // postID|userID|item|count|cost|dateposted
+            //   [0] |  [1] |[2] | [3] | [4]|   [5]
+            foreach (string warn in expiringSoon)
+            {
+                var data = warn.Split('|');
+                ulong userID = Convert.ToUInt64(data[1]);
+                var user = Bot.client.GetUser(userID);
+                string postID = data[0];
+                string count = data[3];
+                string item = data[2];
+                string price = data[4];
+                await user.SendMessageAsync("", embed: new InfoEmbed("WARNING: FREE MARKET POSTING EXPIRATION", "This is your one warning that your Free Market posting of " +
+                                                                    $"{count} {item} for {price} coins will be expiring in 24 hours. You may remove this posting for a 25% fee, or it will " +
+                                                                    $"be auctioned off and the coins will go towards slots.").Build());
             }
 
-            foreach(string warn in expiringSoon)
+            bool removed = false;
+            List<string> bids = new List<string>();
+            foreach(string expiry in expired)
             {
-                //ulong userID = 
+                var data = expiry.Split('|');
+                ulong userID = Convert.ToUInt64(data[1]);
+                var user = Bot.client.GetUser(userID);
+                string postID = data[0];
+                string count = data[3];
+                string item = data[2];
+                string price = data[4];
+                await user.SendMessageAsync("", embed: new InfoEmbed("FREE MARKET POSTING EXPIRATION", "This message is to inform you that your Free Market posting of " +
+                                                                    $"{count} {item} for {price} coins has expired. You have not removed it, and now it will " +
+                                                                    $"be auctioned off and the coins will go towards slots.").Build());
+
+                bids.Add(expiry);
+                for (int i = 0; i < posts.Count(); i++)
+                {
+                    if (posts[i].Split('|')[0] == postID)
+                    {
+                        posts[i] = "";
+                        removed = true;
+                    }
+                }
+            }
+
+            if (removed)
+            {
+                File.WriteAllLines("Files/FreeMarket.txt", posts.Where(x => x != ""));
+
+                if (!File.Exists("Files/Bids.txt")) File.WriteAllText("Files/Bids.txt", "");
+                foreach (string bid in bids)
+                {
+                    //finish bids
+                }
             }
         }
     }
